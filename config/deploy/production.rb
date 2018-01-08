2c4ed28e669409feed5f56bbc86f96851cbed4a6
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2018-01-09 00:35:21 +0900

    Change production.rb key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -22,7 +22,7 @@ server '13.230.220.23', user: 'ec2-user', roles: %w{app db web}
 # role :db,  %w{deploy@example.com}
 
 set :ssh_options, {
-   keys: [File.expand_path('~/.ssh/yamazaki_816816.pem)')]
+   keys: [File.expand_path('~/.ssh/aws_git_rsa)')]
 }
 
 # Configuration

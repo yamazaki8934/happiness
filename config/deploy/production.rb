cc89466f36a61ea3d3ad68c18d159329bf581570
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2018-01-08 22:55:15 +0900

    Add for deploy by capistrano3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,7 @@ server '13.230.220.23', user: 'ec2-user', roles: %w{app db web}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-set :ssh_options, keys: '~/.ssh/first_aws_rsa'
+set :ssh_options, keys: '~/.ssh/aws_git_rsa'
 
 # Configuration
 # =============

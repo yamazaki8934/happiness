df94d072a71503399630e443f56ec75eccd98f66
yamazaki8934 <j6145.yamazaki.4232@outlook.jp>
2018-01-12 00:49:23 +0900

    responsive design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,25 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path, notice: 'アカウントが更新されました'
+    else
+      flash.now[:notice] = '更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+   def user_params
+    params.require(:user).permit(:nickname, :email)
+   end
+
 end

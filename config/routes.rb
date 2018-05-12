281215cd7dda1f96da1ad0234fac04f656b25033
m-kawaguchi <m-kawaguchi@good-mgr.com>
2018-05-12 14:58:15 +0900

    ルーティング処理の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } }
+
   resources :toppage, only: [:index]
   resources :users do
     collection do
